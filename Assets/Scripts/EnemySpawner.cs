6ca6356a4ec7c0a99f913a9a46a3075cd7a4bc62
SimonMuehlmann <simon.muehlmann06@htl-salzburg.ac.at>
2023-05-17 16:34:05 +0200

    .

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemy;
+    [SerializeField]
+    private GameObject enemy2;
+    [SerializeField]
+    private float enemyInterval = 3.5f;
+    [SerializeField]
+    private float enemy2Interval = 8f;
+    [SerializeField]
+    private GameObject[] spawningEnemy;
+    
+    void Start()
+    {
+        StartCoroutine(spawnEnemy(enemyInterval, enemy));
+        StartCoroutine(spawnEnemy(enemy2Interval, enemy2));
+    }
+
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, spawningEnemy[Random.Range(0, 4)].transform.position, Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, enemy));
+    }
+    
+
+}
