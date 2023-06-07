f3ba5179d55113e66a7f5914cead2af4f787c447
TobiasNiedermueller <114598146+TobiasNiedermueller@users.noreply.github.com>
2023-06-07 17:40:28 +0200

    .
    -

--- Assets/Scripts/Scoremanager.cs
+++ Assets/Scripts/Scoremanager.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.UI;
+using TMPro;
 
 public class Scoremanager : MonoBehaviour
 {
     public int score;
     int finalscore;
-    
+    [SerializeField] GameObject scoreN, scoreT, highscoreN, highscoreT;
+    public TextMeshProUGUI scoreText, highscoreText, finalscoreText;
+
     public void End()
     {
+        finalscore = score;
+
+        scoreN.SetActive(true);
+        highscoreN.SetActive(true);
+        scoreT.SetActive(true);
+        highscoreT.SetActive(true);
+
         if (finalscore >= PlayerPrefs.GetInt("Highscore"))
         {
             PlayerPrefs.SetInt("Highscore", finalscore);
         }
+
+        highscoreText.text = PlayerPrefs.GetInt("Highscore").ToString();
+        finalscoreText.text = finalscore.ToString();
+    }
+
+    private void Update()
+    {
+        scoreText.text = score.ToString();
     }
 
 }
+
